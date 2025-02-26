10d47aa984dede99713182ebf413620264fa14f7
Elkjdsvn <quentinmorali@gmail.com>
2025-02-26 15:59:23 +0100

    repo initialization

---
+++ src/main/java/app/obstacles/App.java
@@ -0,0 +1,13 @@
+package app.obstacles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
